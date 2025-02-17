8207bbe3a235ccd9035de366c285d1a06bef9568
Anton Polieshchuk <anton2020ua@gmail.com>
2025-02-17 02:19:11 -0800

    init commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,22 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
 import App from './App';
+import {Component} from "react";
+
+class AppWithTitle extends Component{
+  componentDidMount(){
+    document.title = "Nine or Ten"
+  }
+
+  render(){
+    return(<App />)
+  }
+}
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
-      <App />
+      <AppWithTitle />
     </StyledEngineProvider>
   </React.StrictMode>
 );
