919d930c87a0a18d4079c009183cbc43f97d7006
Anton Polieshchuk <anton2020ua@gmail.com>
2025-02-28 10:43:34 -0800

    limit pagination to 15

--- src/Members.tsx
+++ src/Members.tsx
@@ -128,9 +128,9 @@ export default function MembersList(props: { disableCustomTheme?: boolean }) {
                   params.indexRelativeToCurrentPage % 2 === 0 ? 'even' : 'odd'
                 }
                 initialState={{
-                  pagination: { paginationModel: { pageSize: 25 } },
+                  pagination: { paginationModel: { pageSize: 15 } },
                 }}
-                pageSizeOptions={[25]}
+                pageSizeOptions={[15]}
                 disableColumnResize
                 disableColumnMenu
                 disableColumnSorting
